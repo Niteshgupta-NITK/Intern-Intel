3137acb6496abb50f0ac9a2b0d3f60e654bfcbbb
Niteshgupta-NITK <guptanitesh400@gmail.com>
2022-02-27 09:19:00 +0530

    cors added

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,14 @@ require("./db/mongoose");
 const app = express();
 const userrouter =require('./routes/user');
 require('dotenv').config()
+const cors = require("cors");
+
 
 const port = process.env.PORT;
 
 app.use(express.json());
 app.use(userrouter);
-
+app.use(cors());
 
 app.listen(port, () => {
   console.log("server is up running at port "+ port);
